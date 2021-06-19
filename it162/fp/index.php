a7f3d92fc8acb0948d471a7b50f361235d99ecda
heosman <77591083+heosman@users.noreply.github.com>
2021-06-19 03:03:19 -0700

    Add files via upload

--- it162/fp/index.php
+++ it162/fp/index.php
@@ -2,14 +2,29 @@
 
 <!-- START LEFT COL -->
 <section>
-    <h2 class="pageID">My Final Project</h2>
-    <p>Final project coming soon!</p>
-
+  <h2 class="pageID"><?=$PageID?></h2>
+  <p>Welcome to Osman's Decor!</p><br>
+  <div>
+    <img src="images/sew-machine.jpg" alt="Photo of a sewing machine" class="round">
+  </div>
 </section>
+
 <!-- END LEFT COL -->
 
 <!-- START RIGHT COL -->
+<aside>
+  <p>Here you get a peek into what Osman's Decor is and all the possibilities you can bring into your home.</p>
+
+  <div class="slideshow">
+    <img class="slide" src="images/2.png">
+    <img class="slide" src="images/sofa.png">
+    <img class="slide" src="images/8.png">
+    <img class="slide" src="images/6.png">
+  </div>
+    <a href="gallery.php" class="linkbutton"> View Gallery</a><br><br>
+    <a href="details.php" class="linkbutton"> View Details</a>
+</aside>
 
 <!-- END RIGHT COL -->
- 
+
 <?php include "includes/footer.php";?>
